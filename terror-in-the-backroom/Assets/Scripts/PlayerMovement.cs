0053ffc105203ef421ea49e2cc6216c07c280917
Jayda <jaydapizzuco@gmail.com>
2024-12-17 11:23:06 -0500

    Finished resolving merge errors with audio, pause menu and open/closing inventory + controlling camera movement :'(

--- terror-in-the-backroom/Assets/Scripts/PlayerMovement.cs
+++ terror-in-the-backroom/Assets/Scripts/PlayerMovement.cs
@@ -64,21 +64,18 @@ public class PlayerMovement : MonoBehaviour
         {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
-            // We only allow the mouse to rotate the camera if the inventory is not active
-            if (!inventoryStatus)
-            {
-                LookAround();
-            }
-
-            // Check if player is trying to attack a skeleton
-            CheckForMallet();
-
-            // Open the inventory if the player is pressing I
-            OpenCloseInventory();
-
-            // Drop off items if player is pressing V
-            DropOffItems();
+            LookAround();
         }
+
+        //Check if player is trying to attack a skeleton
+        CheckForMallet();
+
+        //Open the inventory if the player is pressing I
+        OpenCloseInventory();
+
+        //Drop Off Items if player is pressing V
+        DropOffItems();
+    }
 
         void HandlePlayerMovement()
         {
@@ -348,4 +345,4 @@ public class PlayerMovement : MonoBehaviour
 
 
     }
-}
+
